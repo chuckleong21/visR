dd44ef36bde46b10261758984beb907040f40b95
Chuck <chuckleong21@gmail.com>
2019-09-18 17:22:14 +0800

    analysis by month

--- 2019HKsuicide/2019HKsuicide.R
+++ 2019HKsuicide/2019HKsuicide.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(lubridate)
-library(ggalluvial)
 
 df_districts <- read_csv(here::here("2019HKsuicide", "Hong_Kong_18_Districts.csv"))
 
@@ -36,6 +35,11 @@ df_record %<>%
         filter(!is.na(reason)) %>% 
         select(-source, -time, -reason_n, -day_interval)
 
+# ----1----
+# |=================================================================|
+# |                     demographic alluvial                        |
+# |=================================================================|
+library(ggalluvial)
 df_demographic <- 
         select(df_record, date, sex, age, reason) %>% 
         filter(!reason == "不詳", age >= 18) %>% 
@@ -52,24 +56,82 @@ subtitle <- "香港2019年度自殺個案自3月份有明顯上升，6、7月份
 除最主要的健康問題外，其中年輕人多為感情問題而有自殺念頭，中年人則為財政生活問題所困。"
 caption <- "**數據來源：2019香港自殺資料統計** | 設計：@chucc900<br><span style='color:#F57D7D'> 如需幫助請致電香港撒瑪利亞防止自殺會：2389 - 2222</span>"
 
-# demographic alluvial
-ggplot(df_demographic, aes(axis1 = sex, axis2 = age, y = freq)) + 
-        scale_x_discrete(limits = c("", ""), expand = c(-.3, 0.45)) +
-        geom_flow(aes(fill = reason)) + 
-        geom_stratum() + 
-        geom_text(stat = "stratum", label.strata = TRUE, family = "STXihei") + 
-        scale_fill_brewer(name = "主因", type = "qual", palette = "Set3") +
-        facet_wrap(~month, scales = "free_y", nrow = 3) + 
+(
+        ggplot(df_demographic, aes(axis1 = sex, axis2 = age, y = freq)) + 
+                scale_x_discrete(limits = c("", ""), expand = c(-.3, 0.45)) +
+                geom_flow(aes(fill = reason)) + 
+                geom_stratum() + 
+                geom_text(stat = "stratum", label.strata = TRUE, family = "STXihei") + 
+                scale_fill_brewer(name = "主因", type = "qual", palette = "Set3") +
+                facet_wrap(~month, scales = "free_y", nrow = 3) + 
+                theme_minimal() + 
+                labs(x = "", y = "", 
+                     title = "香港2019自殺個案情況", 
+                     subtitle = subtitle, 
+                     caption = caption) + 
+                theme(plot.margin = margin(20, 10, 20, 10), 
+                      plot.title = element_text(face = "bold", size = 24), 
+                      plot.subtitle = element_text(size = 16), 
+                      plot.caption = ggtext::element_markdown(size = 12),
+                      text = element_text(family = "STXihei"), 
+                      panel.grid = element_blank(), 
+                      strip.text = element_text(size = 15)) -> alluvial
+)
+if(!file.exists(here::here("2019HKsuicide", "alluvial.png")))
+        ggsave(here::here("2019HKsuicide", "alluvial.png"), alluvial, 
+               width = 36.9, height = 19.9, units = "cm")
+
+
+# ----2----
+# |============================================|
+# |                     time                   |
+# |============================================|
+library(ggridges)
+df_time <- df_record %>% 
+        filter(deceased == "Yes") %>% 
+        select(date, week, day, age, hour)
+
+ridge <- df_time %>% 
+        group_by(month = month(date)) %>% 
+        count(date) %>% 
+        ungroup() %>% 
+        ggplot(aes(x = n, y = factor(month))) + 
+        geom_density_ridges(aes(fill = factor(month)), alpha = .6, show.legend = FALSE) 
+
+
+ridge_density_lines <- 
+        ggplot_build(ridge) %>% 
+        pluck("data", 1) %>% 
+        group_by(group) %>% 
+        filter(density == max(density)) %>% 
+        ungroup()
+
+ridge + geom_segment(data = ridge_density_lines, linetype = 2,
+                     aes(x = x, xend = x, y = ymin, yend = ymin + density * scale * iscale)) + 
+        scale_y_discrete(labels = month.abb[1:9]) + 
+        coord_flip() +
+        labs(x = "", y = "") +
         theme_minimal() + 
-        labs(x = "", y = "", 
-             title = "香港2019自殺個案情況", 
-             subtitle = subtitle, 
-             caption = caption) + 
-        theme(plot.margin = margin(20, 10, 20, 10), 
-              plot.title = element_text(face = "bold", size = 24), 
-              plot.subtitle = element_text(size = 16), 
-              plot.caption = ggtext::element_markdown(size = 12),
-              text = element_text(family = "STXihei"), 
+        theme(plot.margin = margin(t = 20), 
               panel.grid = element_blank(), 
-              strip.text = element_text(size = 15))
-ggsave(here::here("alluvial"), width = 36.9, height = 19.9, units = "cm")
+              panel.grid.major.y = element_line(color = "grey 80", size = .18))
+
+df_month <- 
+        df_time %>% 
+        group_by(month = month(date, label = TRUE, abbr = TRUE)) %>% 
+        count(date, name = "count") %>% 
+        ungroup() %>% 
+        mutate(quarter = as_factor(case_when(month %in% month.abb[1:3] ~ "1st quarter",
+                                             month %in% month.abb[4:6] ~ "2nd quarter",
+                                             month %in% month.abb[7:9] ~ "3rd quarter")))
+df_month$month <- droplevels(df_month$month)
+
+gplots::plotmeans(count ~ quarter, data = df_month, frame = FALSE, use.t = TRUE, p = .9)
+df_month_aov <- aov(count ~ quarter, data = df_month)
+summary(df_month_aov)
+df_month_aov %>% 
+        TukeyHSD() %>% 
+        pluck("quarter") %>% 
+        as.data.frame() %>% 
+        rownames_to_column(var = "pair")
+
